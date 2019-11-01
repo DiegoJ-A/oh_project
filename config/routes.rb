331f1fa650eacbfaa1c91be698bf661e2d90711b
DiegoJ-A <diego.adorno@hgs.hiddengeniusproject.org>
2019-10-31 17:15:54 -0700

    10/31/19 Office Hours (End) For Save

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
 
+  get '/home_testing', to: 'home#testing'
+  get '/home_index', to: 'home#index'
   resources :guess
 end
