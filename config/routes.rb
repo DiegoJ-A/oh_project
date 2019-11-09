bcc4721ccf4d85e0bd1592ba404cbb9c52341200
DiegoJ-A <diego.adorno@hgs.hiddengeniusproject.org>
2019-11-09 13:41:51 -0800

    11/9/19 Guessing Game Saturday Programming

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
 
+  get '/home_random', to: 'home#random'
   get '/home_testing', to: 'home#testing'
   get '/home_index', to: 'home#index'
   resources :guess
